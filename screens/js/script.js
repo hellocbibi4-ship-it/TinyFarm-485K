d88ee2e5ca9900fd1ad4d9ef37f2e2fd54c026c9
Fanatur <arthur.dixneuf19@gmail.com>
2026-03-17 12:50:00 +0100

    Déplacement de la fonction Horloge du html au js

--- screens/js/script.js
+++ screens/js/script.js
@@ -121,3 +121,16 @@ document.querySelectorAll(".login-language-btn").forEach(btn => {
         panel.classList.toggle("open");
     });
 });
+function updateClock() {
+        const now = new Date();
+
+        const hours = String(now.getHours()).padStart(2, "0");
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        const seconds = String(now.getSeconds()).padStart(2, "0");
+
+        document.getElementById("clock").textContent =
+            `${hours}:${minutes}:${seconds}`;
+    }
+
+    updateClock();
+    setInterval(updateClock, 1000);
